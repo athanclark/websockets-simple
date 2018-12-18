4414616aaaaf560af67cc0c0260d156d35a5b674
Athan Clark <athan.clark@gmail.com>
2018-12-18 10:13:06 -0700

    need to finish reliable a 'la hashing

--- src/Network/WebSockets/Simple/PingPong.hs
+++ src/Network/WebSockets/Simple/PingPong.hs
@@ -2,6 +2,10 @@
     NamedFieldPuns
   , FlexibleContexts
   , OverloadedStrings
+  , GeneralizedNewtypeDeriving
+  , DeriveGeneric
+  , ScopedTypeVariables
+  , DataKinds
   #-}
 
 module Network.WebSockets.Simple.PingPong where
@@ -9,67 +13,69 @@ module Network.WebSockets.Simple.PingPong where
 import Network.WebSockets.Simple (WebSocketsApp (..), WebSocketsAppParams (..))
 
 import Data.Aeson (ToJSON (..), FromJSON (..))
-import Data.Aeson.Types (Value (Array, String), typeMismatch)
-import qualified Data.Vector as V
+import Data.Aeson.JSONMaybe (JSONMaybe (..))
 import Data.Singleton.Class (Extractable (..))
 import Control.Monad (forever)
 import Control.Monad.Trans.Control.Aligned (MonadBaseControl (..))
-import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async (async, cancel)
 import Control.Concurrent.STM (atomically, newEmptyTMVarIO, putTMVar, takeTMVar)
+import Control.Concurrent.Chan.Typed (newChanRW, writeChanRW, readChanRW, ChanRW)
+import Control.Concurrent.Chan.Scope (Scope (Read))
+import Control.Concurrent.Chan.Extra (intersperseStatic, DiffNanosec, readOnly)
+import GHC.Generics (Generic)
 
 
 
 -- | Uses the JSON literal @[]@ as the ping message
-newtype PingPong a = PingPong {getPingPong :: Maybe a}
-
--- | Assumes @a@ isn't an 'Data.Aeson.Types.Array' of anything
-instance ToJSON a => ToJSON (PingPong a) where
-  toJSON (PingPong mx) = case mx of
-    Nothing -> String ""
-    Just x -> toJSON [x]
-
--- | Assumes @a@ isn't an 'Data.Aeson.Types.Array' of anything
-instance FromJSON a => FromJSON (PingPong a) where
-  parseJSON json = case json of
-    String xs
-      | xs == "" -> pure (PingPong Nothing)
-      | otherwise -> fail'
-    Array xs
-      | V.length xs /= 1 -> fail'
-      | otherwise -> (PingPong . Just) <$> parseJSON (xs V.! 0)
-    _ -> fail'
-    where
-      fail' = typeMismatch "PingPong" json
+newtype PingPong a = PingPong
+  { getPingPong :: JSONMaybe a
+  } deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
 
+data PingPongDecay send
+  = PingPongSend send
+  | PingPongReceivedAny
+  | PingPongPing
 
+
+-- | Facilitate a ping-pong enriched websocket, with ping 
 pingPong :: ( MonadBaseControl IO m stM
             , Extractable stM
             )
-         => Int -- ^ Delay in microseconds
+         => DiffNanosec -- ^ Initial delay in microseconds
          -> WebSocketsApp m receive send
          -> m (WebSocketsApp m (PingPong receive) (PingPong send))
 pingPong delay WebSocketsApp{onOpen,onReceive,onClose} = liftBaseWith $ \_ -> do
-  pingingThread <- newEmptyTMVarIO
+  (outgoing :: ChanRW 'Read (PingPongDecay send)) <- readOnly <$> newChanRW
+  (sendable,writer,listener) <- intersperseStatic delay (pure PingPongPing) outgoing
+
+  let send' x = liftBaseWith $ \_ -> writeChanRW sendable (PingPongSend x)
+      mkParams WebSocketsAppParams{close} =
+        WebSocketsAppParams{send = send', close}
+
+  sendingThread <- newEmptyTMVarIO
 
   pure WebSocketsApp
     { onOpen = \params@WebSocketsAppParams{send} -> do
         liftBaseWith $ \runInBase -> do
-          counter <- async $ forever $ do
-            threadDelay delay
-            runInBase $ send $ PingPong Nothing
-          atomically $ putTMVar pingingThread counter
+          sender <- async $ forever $ do
+            ex <- readChanRW outgoing
+            runInBase $ case ex of
+              PingPongPing -> send (PingPong JSONNothing)
+              PingPongSend x -> send $ PingPong $ JSONJust x
+              _ -> pure ()
+          atomically (putTMVar sendingThread sender)
         onOpen (mkParams params)
-    , onReceive = \params (PingPong mPingPong) ->
+    , onReceive = \params (PingPong mPingPong) -> do
+        liftBaseWith $ \_ -> writeChanRW sendable PingPongReceivedAny
         case mPingPong of
-          Nothing -> pure ()
-          Just r -> onReceive (mkParams params) r
+          JSONNothing -> pure ()
+          JSONJust r -> onReceive (mkParams params) r
     , onClose = \o e -> do
         liftBaseWith $ \_ -> do
-          thread <- atomically (takeTMVar pingingThread)
+          thread <- atomically (takeTMVar sendingThread)
           cancel thread
+          cancel writer
+          cancel listener
         onClose o e
     }
-  where
-    mkParams WebSocketsAppParams{send,close} = WebSocketsAppParams{send = send . PingPong . Just,close}
